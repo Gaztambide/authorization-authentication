c1ed115475b45352fa2097b7289be16eeecda4c3
Gaztambide <francogaztambide@gmail.com>
2016-02-22 11:14:19 -0500

    added log in form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
+  root 'sessions#new'
+  resource :sessions
   resource :users
-  
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
